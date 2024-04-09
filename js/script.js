a5c8268241d6b6dfceaf78300f39db9898e681e5
Victor Pimentel Correia <victorpimentelw@gmail.com>
2024-04-08 22:24:14 -0300

    new scroll effect and new scroll bar

--- js/script.js
+++ js/script.js
@@ -7,6 +7,8 @@ const frontendBtn = document.getElementById('frontend-btn');
 const side = document.getElementById('side');
 const sideBtn = document.getElementById('side-btn');
 
+const themeIcon = document.getElementById('toggleTheme');
+
 backendBtn.addEventListener("click", (event) => {
     frontend.style.display = "none";
     side.style.display = "none";
@@ -50,6 +52,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     toggleThemeButton.addEventListener("click", () => {
         const isDark = document.body.classList.toggle("dark-theme");
+        themeIcon.classList.toggle("ri-sun-line");
         localStorage.setItem("isDark", isDark);
     });
 
@@ -59,4 +62,22 @@ document.addEventListener("DOMContentLoaded", () => {
     };
 
     loadTheme();
-})
+});
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+});
+
+sr.reveal(`.profile_border`);
+sr.reveal(`header`);
+sr.reveal(`.profile-name`, {delay: 500});
+sr.reveal(`.profile-role`, {delay: 600});
+sr.reveal(`.perfil-info`, {delay: 700});
+sr.reveal(`.perfil-info`, {interval: 100, delay: 700});
+sr.reveal(`.experience`, {delay: 800});
+sr.reveal(`.cta`, {delay: 900});
+sr.reveal(`.skills`, {delay: 1000});
+sr.reveal(`.portfolio-skills`, {delay: 100});
